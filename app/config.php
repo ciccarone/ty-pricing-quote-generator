9dfcff5e77d6e8344ddf42a44ed8db10c31d5038
Tony Ciccarone <antonio.ciccarone@trustyou.net>
2018-07-26 10:38:50 -0400

    Connect to database and initialize app

--- app/config.php
+++ app/config.php
@@ -1,1 +1,10 @@
-<?php 
+<?php
+
+  // Load env variables
+  $dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
+  $dotenv->load();
+
+  // Connect to DB
+  ORM::configure('mysql:host='.getenv('DB_HOST').';dbname='.getenv('DB_NAME').'');
+  ORM::configure('username', getenv('DB_USER'));
+  ORM::configure('password', getenv('DB_PASS'));
---
+++ app/init.php
@@ -0,0 +1,4 @@
+<?php
+
+  // Initialize app info
+  $app = ORM::for_table('app')->find_one();
